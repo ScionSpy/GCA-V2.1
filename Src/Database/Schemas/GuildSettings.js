d59a4ec5d238fb14d8a57ca98451f7138df60b59
§π∆d°≈~§¶¥#1904 <29105063+ScionSpy@users.noreply.github.com>
2025-04-03 18:40:03 -0400

    add "name" to GuildSettings

--- Src/Database/Schemas/GuildSettings.js
+++ Src/Database/Schemas/GuildSettings.js
@@ -5,6 +5,7 @@
  * @typedef {Object} GuildSettingsSchema
  *
  * @property {String} id Discord GuildId
+ * @property {String} name Discord Guild's name.
  * @property {Boolean} available A flag indicating this client's presence within the guild.
  * @property {String} prefix This Guild's client prefix.
  * @property {String} invite This Guilds invite created by the client.
