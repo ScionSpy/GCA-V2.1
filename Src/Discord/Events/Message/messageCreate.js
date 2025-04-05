713c184b9d36b570ac7b157ac92c7859217b5e89
§π∆d°≈~§¶¥#1904 <29105063+ScionSpy@users.noreply.github.com>
2025-04-05 01:39:00 -0400

    Correctly loads un-cached settings, enables "Global Prefix"

--- Src/Discord/Events/Message/messageCreate.js
+++ Src/Discord/Events/Message/messageCreate.js
@@ -10,8 +10,10 @@ module.exports = async (client, message) => {
     if (!message.guild || message.author.bot) return;
 
     let settings = await client.getSettings(message.guild.id);
-
-    if (!settings){
+    if (settings && settings.constructor.name !== "GuildSettings"){
+        settings = new GuildSettings(message.guild, settings);
+    }
+    else if (!settings){
         settings = new GuildSettings(message.guild);
         client.GuildSettings.set(message.guild.id, settings);
         //if (client.supportServer) await client.supportServer.joinedGuild(message.guild);
@@ -22,14 +24,22 @@ module.exports = async (client, message) => {
 
     // check for bot mentions
     if (message.content.startsWith(`<@${client.user.id}>`)) {
-        message.channel.sendSafely(`> My prefix is \`${settings.prefix}\``);
+        message.channel.sendSafely(`> My global prefix is \`${PREFIX}\`\n My ${message.guild.mame} prefix is \`${settings.prefix}\``);
     };
 
-    if (USE_PREFIX_COMMANDS && message.content && message.content.startsWith(settings.prefix)) {
-        const commandName = message.content.replace(`${settings.prefix}`, "").split(/\s+/)[0];
+    if (USE_PREFIX_COMMANDS && message.content &&
+        // Does this message start with the Global {PREFIX} or the guild {settings.prefix} ?
+        (message.content.startsWith(settings.prefix) || message.content.startsWith(PREFIX))
+    ) {
+        let content;
+        // Remove the prefix from the start of the mesage.
+        if (message.content.startsWith(settings.prefix)) content = message.content.replace(settings.prefix, "");
+        else if (message.content.startsWith(PREFIX)) content = message.content.replace(PREFIX, "");
+
+        const commandName = content.split(/\s+/)[0];
         const cmd = client.getCommand(commandName);
 
-        if(!cmd) console.log(client.commands, client.commandIndex)
+        //if(!cmd) console.log(client.commands, client.commandIndex)
         if (cmd) {
             isCommand = true;
             commandHandler.handlePrefixCommand(message, cmd, settings);
