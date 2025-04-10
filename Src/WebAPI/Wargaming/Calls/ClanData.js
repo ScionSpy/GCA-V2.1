b419be36510a27fcad55ed24ca6397a811164142
§π∆d°≈~§¶¥#1904 <29105063+ScionSpy@users.noreply.github.com>
2025-04-10 00:38:38 -0400

    Adds API.Clan search functions

--- Src/WebAPI/Wargaming/Calls/ClanData.js
+++ Src/WebAPI/Wargaming/Calls/ClanData.js
@@ -6,6 +6,66 @@ const ClanData = {};
 module.exports = ClanData;
 
 
+/**
+ * Find a clan by name or tag.
+ * @param {String[] | String} query query to look for.
+ * @returns {Clan_Lookup[]}
+ */
+ClanData.lookup = async function clanLookup(query) {
+    if (typeof query === "number" && query.toString().length >= 10) query = `${query}`;
+    else if (Array.isArray(query)) query = query.join(",");
+    if (typeof query !== "string") throw new Error(`WargamingAPI.Clan.lookup();\n  'query' must be an array of strings, or a string! got ${typeof query}\n`);
+    if (query.length < 2) throw new Error(`WargamingAPI.Clan.lookup();\n  'query' must have a minimum character limit of 2.`);
+
+    let results = await API.makeAPICall('wows/clans/list/', `search=${query}`);
+
+    if (results.status === "error") throw new Error(`WargamingAPI.Clan.lookup(query) -> ` + await API.handelApiError(results.error, 'clans/list'));
+
+    return results.data;
+};
+
+
+/**
+ *
+ * @param {String} query
+ * @param {String} authToken Authorization Token
+ * @returns {Array<Array<Clan_Info>>}
+ */
+ClanData.getDetails = async function getClanDetails(query, authToken) {
+    if (typeof query === "number" && query.toString().length >= 10) query = `${query}`;
+    else if (Array.isArray(query)) query = query.join(",");
+    if (typeof query !== "string") throw new Error(`WargamingAPI.Clan.getDetails();\n  'query' must be an array of strings, or a string! got ${typeof query}\n`);
+    if (typeof authToken !== "string") throw new Error(`WargamingAPI.Clan.getDetails(); 'authToken' must be a string! got ${typeof authToken}`);
+
+    let queryData = [[]]
+    // in the event we're trying to lookup more than 100 clans, devide them up into groups of 100.
+    // // WGAPI limits the query to 100.
+    if (query.includes(',')) queryData = defineQuery(query, 100);
+    else queryData[0][0] = query; // "startswith"
+
+    /**
+     * @type {Clan_Info[]}
+     */
+    let clans = [];
+
+    for (let x = 0; x < queryData.length; x++) {
+
+        // Verify that every entity is a Number, defined as a string.
+        for (let y = 0; y < queryData[x].length; y++) {
+            let q = queryData[x][y];
+            if (isNaN(q)) throw new Error(`WargamingAPI.Clan.getDetails(query = '${q}')\n  queries must be a string of Numbers! got ${typeof q} : ${q}\n`);
+        };
+
+        let results = await API.makeAPICall('wows/clans/info', `extra=members&clan_id=${queryData[x].join(',')}`, authToken);
+
+        if (results.status === "error") throw new Error(`WargamingAPI.Clan.getDetails(query='${query}') -> Sent query=${results.error.value}` + await API.handelApiError(results.error, 'clans/info'));
+        else clans = clans.concat(results.data);
+    };
+
+    return clans;
+};
+
+
 //#region Clan Applications
 
 /**
@@ -15,12 +75,9 @@ module.exports = ClanData;
 ClanData.getApplications = async function getInviteApplications(authToken) {
     let query = "battle_type=pvp&order=-updated_at&offset=0&limit=100";
 
-    let auth = process.env.API_WARGAMING_ADMINAUTH ? process.env.API_WARGAMING_ADMINAUTH : authToken;
-    if (!auth) throw new Error('\'ClanData.getApplications(authToken);\' an Authorization Token is required for this function!');
+    if (!authToken) throw new Error('\'ClanData.getApplications();\' an Authorization Token is required for this function!');
 
-    let Cookies = `wsauth_token=${auth}`;
-
-    let results = await API.makeAPICall('api/recruitment/active_clan_applications/', query, Cookies);
+    let results = await API.makeAPICall('api/recruitment/active_clan_applications/', query, authToken);
 
     if (results.applications) return results.applications;
     else return results;
@@ -97,22 +154,18 @@ __declineApplicationError = async function declineApplicationError(results) {
 };
 
 /**
- * @param {String} authToken Authorization Token
  * @param {Object} data
  * @param {Number} data.id
  * @param {String} data.status Status to send.
  * * "Accepted" | "declined"
+ * @param {String} authToken Authorization Token
  * @returns {Application.Conf|Application.ConfErr}
  */
-ClanData.sendApplicationResponse = async function sendApplicationResponse(authToken, data) {
+ClanData.sendApplicationResponse = async function sendApplicationResponse(data, authToken) {
 
-    let auth = process.env.API_WARGAMING_ADMINAUTH ? process.env.API_WARGAMING_ADMINAUTH : authToken;
-    if (!auth) throw new Error('\'ClanData.sendApplicationResponse(authToken, data);\' an Authorization Token is required for this function!');
+    if (!authToken) throw new Error('\'ClanData.sendApplicationResponse(authToken, data);\' an Authorization Token is required for this function!');
 
-    let Cookies = `wsauth_token=${auth}`;
-
-
-    let results = await API.makeApiCall_PATCH('api/recruitment/applications/' + data.id + '/', { status: data.status }, Cookies);
+    let results = await API.makeApiCall_PATCH('api/recruitment/applications/' + data.id + '/', { status: data.status }, authToken);
 
     if (results.title || results.error) {
         if (data.status === "accepted") return await __acceptApplicationError(results);
