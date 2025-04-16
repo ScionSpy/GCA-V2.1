003af4e47540b9d1943a302f911ae2fd2cb9113e
§π∆d°≈~§¶¥#1904 <29105063+ScionSpy@users.noreply.github.com>
2025-04-15 21:49:39 -0400

    Resolves an error.
    Not sure why this was added, removed to prevent a modular error when accessing this file

--- Src/Discord/Structures/index.js
+++ Src/Discord/Structures/index.js
@@ -1,10 +1,8 @@
 const BotClient = require("./BotClient");
 const Command = require("./Command");
 const CommandCategory = require("./CommandCategory");
-const BaseContext = require("./BaseContext");
 
 module.exports = {
-    BaseContext,
     BotClient,
     Command,
     CommandCategory,
